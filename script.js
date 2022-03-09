c5b21addc70f6a1b95071ad74f42c4469472b813
Vikrant Malla <vikrantmalla999@gmail.com>
2022-03-09 14:52:11 +0545

    solve accessibility issue

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const toggle = document.getElementById('toggle');
-const price = document.getElementById('price');
+const basicPrice = document.querySelector('.basic-price');
+const proPrice = document.querySelector('.pro-price');
+const masterPrice = document.querySelector('.master-price');
 
-toggle.addEventListener('change', e => {
-    price.classList.toggle('show-monthly');
+function priceChange(e) {
+    if (e.target.checked) {
+        basicPrice.innerText = "$19.99";
+        proPrice.innerText = "$24.99";
+        masterPrice.innerText = "$39.99";
+    } else {
+        basicPrice.innerText = "$199.99";
+        proPrice.innerText = "$249.99";
+        masterPrice.innerText = "$399.99";
+    };
+}
+
+toggle.addEventListener('change', (e) => {
+    priceChange(e)
 });
